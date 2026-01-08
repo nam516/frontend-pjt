b4f68887be9e4f95b6ed371c3b9008ecc40e2332
retail-tech <nam516@retailtech.co.kr>
2026-01-08 16:58:23 +0900

    20260108 최재준 redirect 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,26 @@ export default defineConfig({
             "@": path.resolve(__dirname, "src"),
         },
     },
+    // vite.config.ts
+    server: {
+        proxy: {
+            // OAuth2 로그인 시작
+            "/oauth2/authorization": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            // OAuth2 콜백
+            "/login/oauth2/code": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 });
